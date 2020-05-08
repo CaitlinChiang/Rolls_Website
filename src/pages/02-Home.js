fd72d0d601c9680cc84d08e434693b34d5c57fe6
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2020-05-08 20:44:08 +0000

    additions to the program

--- src/pages/02-Home.js
+++ src/pages/02-Home.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
 
+function Slider() {
+	return (
+		<div id="slider">
+			<figure>
+				<img src="/images/bg1.jpg" />
+				<img src="/images/bg2.jpg" />
+				<img src="/images/bg3.jpg" />
+				<img src="/images/bg4.jpg" />
+				<img src="/images/bg1.jpg" />
+			</figure>
+		</div>
+	)
+}
+
 function Home() {
 	return (
 		<div>
 			<section id="home">
+				{Slider()}
 				<div class="container fade-in">
 					<h1>ROLLS</h1>
-					<h4>Caption</h4>
 				</div>
 				<a href="#about"><img class="downArrow" src="https://image.flaticon.com/icons/svg/2316/2316598.svg" /></a>
 			</section>
