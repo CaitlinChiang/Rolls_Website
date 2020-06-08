b1956e03484c2cadda5b4d2ffe20ce8ebf8e6cb3
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2020-06-08 23:22:11 +0800

    Code Updates

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -13,8 +13,8 @@ class Account extends Component {
 	uiConfig = {
 		signInFlow: 'popup',
 		signInOptions: [
-			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-			firebase.auth.FacebookAuthProvider.PROVIDER_ID
+			firebase.auth.GoogleAuthProvider.PROVIDER_ID
+			
 		],
 		callbacks: {
 			signInSuccess: () => false		
